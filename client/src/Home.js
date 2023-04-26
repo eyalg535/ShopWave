7e8bab3609ad327ca5c3fec4e8815affb1631b66
eyal gerby <eyalg535@gmail.com>
2023-04-25 21:59:03 -0400

    creating Home component

---
+++ client/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to ShopWave</h1>
+      <p>Find great deals on a wide variety of products from independent sellers.</p>
+      <p>Start browsing now!</p>
+    </div>
+  );
+}
+
+export default Home;
