338c20c8ea260d6e215c200a73b6b7eeeffb22da
eyal gerby <eyalg535@gmail.com>
2023-04-26 22:02:44 -0400

    add css and updates for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import logo from './ShopWaveLogo.jpg';
 import './App.css';
-import 
+import  Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './Navbar.css';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          {/* <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} /> */}
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} /> */}
+        </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,30 +1,35 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css';
 
-export default function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/shop">Shop</Link>
-        </li>
-        <li>
-          <Link to="/cart">Cart</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/about">About Us</Link>
-        </li>
-        <li>
-          <Link to="/login">Log In/Sign Up</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+function CustomNavbar() {
+const brandStyles = {
+fontSize: '32px', // increase the font size
+fontFamily: 'cursive', // change the font to a more wave-like font
+fontWeight: 'bold', // add some extra weight to the font
+color: '#0077be', 
+};
+
+return (
+<Navbar bg="light" expand="lg">
+<Navbar.Brand href="/" style={brandStyles}>ShopWave</Navbar.Brand>
+<Navbar.Toggle aria-controls="basic-navbar-nav" />
+<Navbar.Collapse id="basic-navbar-nav">
+<Nav className="mr-auto">
+<Nav.Link as={Link} to="/">Home</Nav.Link>
+<Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+<Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+<Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+<Nav.Link as={Link} to="/about">About Us</Nav.Link>
+</Nav>
+<Nav>
+<Nav.Link as={Link} to="/login">Log In/Sign Up</Nav.Link>
+</Nav>
+</Navbar.Collapse>
+</Navbar>
+);
 }
 
-
+export default CustomNavbar;
