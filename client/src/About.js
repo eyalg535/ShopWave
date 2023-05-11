7736a0ea9d3019df3de239d4dfbeb2427bab56ca
eyal gerby <eyalg535@gmail.com>
2023-05-10 20:22:10 -0400

    remove junk

--- client/src/About.js
+++
@@ -1,9 +0,0 @@
-export default function About() {
-    return (
-        <div>
-            <h2>About Us</h2>
-            <p>Welcome to our online marketplace! We provide a platform for users to buy and sell goods in a secure and convenient way.</p>
-            <p>Contact us at support@marketplace.com if you have any questions or concerns.</p>
-        </div>
-    );
-}
