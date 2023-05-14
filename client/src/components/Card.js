706dfffc4e87df23e7bafb6971a5e55357c3bb5c
eyal gerby <eyalg535@gmail.com>
2023-05-13 23:06:33 -0400

    create card comp

---
+++ client/src/components/Card.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Card({ 
+  product,
+  title,
+  description,
+  image,
+  image1,
+  image2,
+  image3,
+  price,
+  carts,
+  currentUserId,
+  reviews,
+  setCarts,
+  setAddedToCart,
+  getProductById,
+}) {
+  
+  const addToCart = async () => {
+    const newProd = await getProductById(product.id); // Retrieve product by ID
+    const cart = carts.length === 1 ? carts[0] : carts.slice(-1)[0];
+    const res = await fetch(`/cart/${cart.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user_id: cart.user_id,
+        products: newProd,
+      }),
+    });
+    if (res.ok) {
+      const updatedCart = await res.json();
+      setCarts(
+        carts.map((cart) => (cart.id === updatedCart.id ? updatedCart : cart))
+      );
+      setAddedToCart(true);
+      setTimeout(() => setAddedToCart(false), 5000);
+    }
+  };
+
+  const handleAddToCart = async () => {
+    if (!currentUserId) {
+      alert("Please login to add to cart!");
+      return;
+    }
+    console.log("add to cart");
+    await addToCart();
+  };
+
+  return (
+    <div className="col mb-4">
+      <div className="card" style={{ width: "18rem" }}>
+          <img src={image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text"></p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            Price: $
+            {price}
+          </li>
+        </ul>
+        <div className="card-body">
+          <Link to={{ pathname: `/show/${product.id}` , state: { product: { title, description, image, price } } }}>
+            <button type="button" className="btn btn-primary mr-2">
+              See more...
+            </button>
+          </Link>
+          <button
+            className="btn btn-warning"
+            onClick={handleAddToCart}
+          >
+            <i className="fas fa-cart-plus">Add to cart</i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
