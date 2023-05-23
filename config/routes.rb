0796eb2670927af15f0e5020cb6260ab96b28815
eyal gerby <eyalg535@gmail.com>
2023-05-23 17:24:28 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,25 @@ Rails.application.routes.draw do
   resources :products
   resources :carts
   resources :users
-  post("/login", to: "sessions#create")
-  post '/signup', to: 'users#create'
-  patch '/cart/:id', to: 'carts#update'
-  get("/users", to: "users#index")
-  get("/users/:id", to: "users#show")
-  get("/cart/:id", to: "carts#show")
-  # get("/users/:id/cart/id:/order", to: "orders#show")
-  get '/users/:id/carts', to: 'carts#checkout'
 
-  get("/products", to: "products#index")
-  get("/products/:id", to: "products#show")
-  delete("/logout", to: "sessions#destroy")
-  # get '/cart/:id', to: "cart#checkout"
-  get("/me", to: "users#me")
-  get('users/:id/carts/current_user_cart', to: 'carts#current_user_cart')
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#me"
+
+  get "/users/:id/carts", to: "users#show"
+  get "/users/:id/orders", to: "orders#show"
+  get "/users/:id/carts/checkout", to: "carts#checkout"
+  get "/users/:user_id/carts/current_user_cart", to: "carts#current_user_cart"
+  delete "/users/:id/carts", to: "users#destroy"
+  
+  get "/cart/:id", to: "carts#show"
+  patch "/cart/:id", to: "carts#update"
+  delete "/carts/:id/orders/:order_id", to: "carts#destroy", as: "delete_order"
+  delete "/carts/:id", to: "carts#destroy", as: "empty_cart"
+
+  get "/products", to: "products#index"
+  get "/products/:id", to: "products#show"
+  delete "/products/:id", to: "products#destroy"
 end
+
