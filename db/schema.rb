76d96c68ae1685db994ddde5ed69a8a9ee198d99
eyal gerby <eyalg535@gmail.com>
2023-05-23 17:24:37 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_14_025942) do
+ActiveRecord::Schema.define(version: 2023_05_02_042326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2023_05_14_025942) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.integer "price"
+    t.integer "quantity"
     t.string "description"
     t.string "image"
     t.string "image1"
@@ -39,7 +40,6 @@ ActiveRecord::Schema.define(version: 2023_05_14_025942) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
